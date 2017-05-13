5b5746f9a16f66523e950f3d763241da4f2965d4
zaeval <h3065@naver.com>
2017-05-14 08:54:11 +0900

    [front] third commit
    Timetable add & modal add & friend manage

--- dist/js/offcanvas.js
+++ dist/js/offcanvas.js
@@ -3,3 +3,52 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+var mode=0;
+
+function goToGroupCategory(obj){
+    obj.insertAfter($("#group_category"));
+    $(obj).on("click",
+        function(){
+            goToFriendCategory($(this));
+        }
+    );
+    
+}
+function goToFriendCategory(obj){
+    obj.insertAfter($("#friend_category"));
+    $(obj).on("click",
+        function(){
+            goToGroupCategory($(this));
+        }
+    );
+}
+$(document).ready(function () {
+  for(i in $(".list-group-item")){
+     if(($(".list-group-item")[i]).id=="group_category"){
+        mode=2;
+        continue;
+    }
+    if(($(".list-group-item")[i]).id=="friend_category"){
+        mode=1;
+        continue;
+    }
+    if(($(".list-group-item")[i]).id=="plus"){
+        mode=0;
+    }
+    if(mode==1){
+        $($(".list-group-item")[i]).on("click",
+            function(){
+                goToGroupCategory($(this));
+            }
+        );
+    }
+    if(mode==2){
+        $($(".list-group-item")[i]).on("click",
+            function(){
+                goToFriendCategory($(this));
+            }
+        );
+    }
+  }
+});
