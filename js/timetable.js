f285776a910ec0c2fd2af86ff04531cd3735112a
zaeval <h3065@naver.com>
2017-05-14 14:17:17 +0900

    Success
    ㅇㅇ

--- js/timetable.js
+++ js/timetable.js
@@ -158,3 +158,57 @@ function SchedulePlan( element ) {
 			}, 20);
 		}
 	};
+
+function initSchedule() {
+    var transitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
+	var transitionsSupported = ( $('.csstransitions').length > 0 );
+	//if browser does not support transitions - use a different event to trigger them
+	if( !transitionsSupported ) transitionEnd = 'noTransition';
+	
+	//should add a loding while the events are organized 
+
+	
+
+	var schedules = $('.cd-schedule');
+	var objSchedulesPlan = [],
+		windowResize = false;
+	
+	if( schedules.length > 0 ) {
+		schedules.each(function(){
+			//create SchedulePlan objects
+			objSchedulesPlan.push(new SchedulePlan($(this)));
+		});
+	}
+
+	$(window).on('resize', function(){
+		if( !windowResize ) {
+			windowResize = true;
+			(!window.requestAnimationFrame) ? setTimeout(checkResize) : window.requestAnimationFrame(checkResize);
+		}
+	});
+
+	function checkResize(){
+		objSchedulesPlan.forEach(function(element){
+			element.scheduleReset();
+		});
+		windowResize = false;
+	}
+
+	function getScheduleTimestamp(time) {
+		//accepts hh:mm format - convert hh:mm to timestamp
+		time = time.replace(/ /g,'');
+		var timeArray = time.split(':');
+		var timeStamp = parseInt(timeArray[0])*60 + parseInt(timeArray[1]);
+		return timeStamp;
+	}
+
+	function transformElement(element, value) {
+		element.css({
+		    '-moz-transform': value,
+		    '-webkit-transform': value,
+			'-ms-transform': value,
+			'-o-transform': value,
+			'transform': value
+		});
+	}
+}
