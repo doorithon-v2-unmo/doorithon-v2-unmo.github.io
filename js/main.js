c948d70c47227ece1a4c26e99925a7b0223d0a4c
zaeval <h3065@naver.com>
2017-05-14 12:57:02 +0900

    test7
    Test7

--- js/main.js
+++ js/main.js
@@ -95,6 +95,11 @@ function ajaxSubmit() {
     for(var i in global_groups_list){
         global_groups_id_list.push(global_groups_list[i].userid);
     }
+    for(var i in global_groups_id_list){
+        if(global_groups_id_list[i]==null){
+            global_groups_id_list.splice(i,1);
+        }
+    }
     console.log(global_groups_id_list);
     $.ajax({
         type: "POST",
