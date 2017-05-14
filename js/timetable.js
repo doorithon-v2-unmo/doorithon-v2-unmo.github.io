09b1b4b59b28dc44d9fd16d0a4608fcfb80595bf
zaeval <h3065@naver.com>
2017-05-14 14:10:39 +0900

    As
    As

---
+++ js/timetable.js
@@ -0,0 +1,160 @@
+function SchedulePlan( element ) {
+		this.element = element;
+		this.timeline = this.element.find('.timeline');
+		this.timelineItems = this.timeline.find('li');
+		this.timelineItemsNumber = this.timelineItems.length;
+		this.timelineStart = getScheduleTimestamp(this.timelineItems.eq(0).text());
+		//need to store delta (in our case half hour) timestamp
+		this.timelineUnitDuration = getScheduleTimestamp(this.timelineItems.eq(1).text()) - getScheduleTimestamp(this.timelineItems.eq(0).text());
+
+		this.eventsWrapper = this.element.find('.events');
+		this.eventsGroup = this.eventsWrapper.find('.events-group');
+		this.singleEvents = this.eventsGroup.find('.single-event');
+		this.eventSlotHeight = this.eventsGroup.eq(0).children('.top-info').outerHeight();
+
+		this.modal = this.element.find('.event-modal');
+		this.modalHeader = this.modal.find('.header');
+		this.modalHeaderBg = this.modal.find('.header-bg');
+		this.modalBody = this.modal.find('.body'); 
+		this.modalBodyBg = this.modal.find('.body-bg'); 
+		this.modalMaxWidth = 800;
+		this.modalMaxHeight = 480;
+
+		this.animating = false;
+
+		this.initSchedule();
+	}
+
+	SchedulePlan.prototype.initSchedule = function() {
+		this.scheduleReset();
+		this.initEvents();
+	};
+
+	SchedulePlan.prototype.scheduleReset = function() {
+		var mq = this.mq();
+		if( mq == 'desktop' && !this.element.hasClass('js-full') ) {
+			//in this case you are on a desktop version (first load or resize from mobile)
+			this.eventSlotHeight = this.eventsGroup.eq(0).children('.top-info').outerHeight();
+			this.element.addClass('js-full');
+			this.placeEvents();
+			this.element.hasClass('modal-is-open') && this.checkEventModal();
+		} else if(  mq == 'mobile' && this.element.hasClass('js-full') ) {
+			//in this case you are on a mobile version (first load or resize from desktop)
+			this.element.removeClass('js-full loading');
+			this.eventsGroup.children('ul').add(this.singleEvents).removeAttr('style');
+			this.eventsWrapper.children('.grid-line').remove();
+			this.element.hasClass('modal-is-open') && this.checkEventModal();
+		} else if( mq == 'desktop' && this.element.hasClass('modal-is-open')){
+			//on a mobile version with modal open - need to resize/move modal window
+			this.checkEventModal('desktop');
+			this.element.removeClass('loading');
+		} else {
+			this.element.removeClass('loading');
+		}
+	};
+
+	SchedulePlan.prototype.initEvents = function() {
+		var self = this;
+
+		this.singleEvents.each(function(){
+			//create the .event-date element for each event
+			var durationLabel = '<span class="event-date">'+$(this).data('start')+' - '+$(this).data('end')+'</span>';
+			$(this).children('a').prepend($(durationLabel));
+
+			//detect click on the event and open the modal
+			$(this).on('click', 'a', function(event){
+				event.preventDefault();
+			});
+		});
+	};
+
+	SchedulePlan.prototype.placeEvents = function() {
+		var self = this;
+		this.singleEvents.each(function(){
+			//place each event in the grid -> need to set top position and height
+			var start = getScheduleTimestamp($(this).attr('data-start')),
+				duration = getScheduleTimestamp($(this).attr('data-end')) - start;
+
+			var eventTop = self.eventSlotHeight*(start - self.timelineStart)/self.timelineUnitDuration,
+				eventHeight = self.eventSlotHeight*duration/self.timelineUnitDuration;
+			
+			$(this).css({
+				top: (eventTop -1) +'px',
+				height: (eventHeight+1)+'px'
+			});
+		});
+
+		this.element.removeClass('loading');
+	};
+
+	SchedulePlan.prototype.mq = function(){
+		//get MQ value ('desktop' or 'mobile') 
+		var self = this;
+		return window.getComputedStyle(this.element.get(0), '::before').getPropertyValue('content').replace(/["']/g, '');
+	};
+
+	SchedulePlan.prototype.checkEventModal = function(device) {
+		this.animating = true;
+		var self = this;
+		var mq = this.mq();
+
+		if( mq == 'mobile' ) {
+			//reset modal style on mobile
+			self.modal.add(self.modalHeader).add(self.modalHeaderBg).add(self.modalBody).add(self.modalBodyBg).attr('style', '');
+			self.modal.removeClass('no-transition');	
+			self.animating = false;	
+		} else if( mq == 'desktop' && self.element.hasClass('modal-is-open') ) {
+			self.modal.addClass('no-transition');
+			self.element.addClass('animation-completed');
+			var event = self.eventsGroup.find('.selected-event');
+
+			var eventTop = event.offset().top - $(window).scrollTop(),
+				eventLeft = event.offset().left,
+				eventHeight = event.innerHeight(),
+				eventWidth = event.innerWidth();
+
+			var windowWidth = $(window).width(),
+				windowHeight = $(window).height();
+
+			var modalWidth = ( windowWidth*.8 > self.modalMaxWidth ) ? self.modalMaxWidth : windowWidth*.8,
+				modalHeight = ( windowHeight*.8 > self.modalMaxHeight ) ? self.modalMaxHeight : windowHeight*.8;
+
+			var HeaderBgScaleY = modalHeight/eventHeight,
+				BodyBgScaleX = (modalWidth - eventWidth);
+
+			setTimeout(function(){
+				self.modal.css({
+					width: modalWidth+'px',
+					height: modalHeight+'px',
+					top: (windowHeight/2 - modalHeight/2)+'px',
+					left: (windowWidth/2 - modalWidth/2)+'px',
+				});
+				transformElement(self.modal, 'translateY(0) translateX(0)');
+				//change modal modalBodyBg height/width
+				self.modalBodyBg.css({
+					height: modalHeight+'px',
+					width: '1px',
+				});
+				transformElement(self.modalBodyBg, 'scaleX('+BodyBgScaleX+')');
+				//set modalHeader width
+				self.modalHeader.css({
+					width: eventWidth+'px',
+				});
+				//set modalBody left margin
+				self.modalBody.css({
+					marginLeft: eventWidth+'px',
+				});
+				//change modal modalHeaderBg height/width and scale it
+				self.modalHeaderBg.css({
+					height: eventHeight+'px',
+					width: eventWidth+'px',
+				});
+				transformElement(self.modalHeaderBg, 'scaleY('+HeaderBgScaleY+')');
+			}, 10);
+
+			setTimeout(function(){
+				self.modal.removeClass('no-transition');
+				self.animating = false;	
+			}, 20);
+		}
+	};
