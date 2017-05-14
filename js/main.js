735914c137899ded72ba9a44ff43e887a0a28874
zaeval <h3065@naver.com>
2017-05-14 12:30:03 +0900

    [front] test
    Test

--- js/main.js
+++ js/main.js
@@ -90,12 +90,16 @@ function procLogout() {
 
 function ajaxSubmit() {
     showLoader();
+    var global_groups_id_list=new Array();
+    for(var i in global_groups_list){
+        global_groups_id_list.push(global_groups_list[i].userid);
+    }
     $.ajax({
         type: "POST",
         url: "https://unmo.herokuapp.com/service/submit",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        data: JSON.stringify({"session_id": getSessionId(), "users": global_groups_list}),
+        data: JSON.stringify({"session_id": getSessionId(), "users": global_groups_id_list}),
         success: function (data) {
             hideLoader();
 
